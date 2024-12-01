9cc1169d5bb2d68901f52c2161694ecbc1d30ee5
Håkon Schia <hakonschia5@gmail.com>
2024-12-01 09:25:11 +0100

    is this what the kids call functional programming

--- src/main/kotlin/1.kt
+++ src/main/kotlin/1.kt
@@ -7,35 +7,39 @@ import kotlin.math.abs
 fun one() {
     val input = Path("src/main/resources/1.txt").readLines()
 
-    val (first, second) = input
-        .map {
-            it.split("   ").let { (first, second) -> first.toInt() to second.toInt() }
-        }
-        .unzip()
+    val totalDistance = input
+        .splitToLists()
         .let { (first, second) ->
             first.sorted() to second.sorted()
         }
+        .let { (first, second) ->
+            first.foldIndexed(initial = 0) { index, acc, value ->
+                acc + abs(value - second[index])
+            }
+        }
 
-    val total = first.foldIndexed(initial = 0) { index, acc, value ->
-        acc + abs(value - second[index])
-    }
-
-    println("One:\t\t $total")
+    println("One:\t\t $totalDistance")
 }
 
 fun oneHard() {
     val input = Path("src/main/resources/1.txt").readLines()
 
-    val (first, second) = input
+    val similarity = input
+        .splitToLists()
+        .let { (first, second) ->
+            first.fold(initial = 0) { acc, value ->
+                val count = second.count { it == value }
+                acc + count * value
+            }
+        }
+
+    println("One hard:\t $similarity")
+}
+
+private fun List<String>.splitToLists(): Pair<List<Int>, List<Int>> {
+    return this
         .map {
             it.split("   ").let { (first, second) -> first.toInt() to second.toInt() }
         }
         .unzip()
-
-    val total = first.fold(initial = 0) { acc, value ->
-        val count = second.count { it == value }
-        acc + count * value
-    }
-
-    println("One hard:\t $total")
 }
