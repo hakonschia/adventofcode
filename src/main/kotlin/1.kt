65435b5daf5c774007b46a1a88cf9faa9d263241
Håkon Schia <hakonschia5@gmail.com>
2024-12-01 08:06:57 +0100

    one :)

---
+++ src/main/kotlin/1.kt
@@ -0,0 +1,45 @@
+package org.example
+
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+import kotlin.math.abs
+
+fun one() {
+    val input = Path("src/main/resources/1.txt").readLines()
+
+    val (first, second) = input
+        .map {
+            it.split("   ").let {
+                it[0].toInt() to it[1].toInt()
+            }
+        }
+        .unzip()
+        .let { (first, second) ->
+            first.sorted() to second.sorted()
+        }
+
+    val total = first.foldIndexed(initial = 0) { index, acc, value ->
+        acc + abs(value - second[index])
+    }
+
+    println("One:\t\t $total")
+}
+
+fun oneHard() {
+    val input = Path("src/main/resources/1.txt").readLines()
+
+    val (first, second) = input
+        .map {
+            it.split("   ").let {
+                it[0].toInt() to it[1].toInt()
+            }
+        }
+        .unzip()
+
+    val total = first.fold(initial = 0) { acc, value ->
+        val count = second.count { it == value }
+        acc + count * value
+    }
+
+    println("One hard:\t $total")
+}
