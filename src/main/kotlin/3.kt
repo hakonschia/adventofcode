963eb4c5d884131ba06f89c58debc33be9927e52
Håkon Schia <hakonschia5@gmail.com>
2024-12-03 09:09:48 +0100

    Much prettier

--- src/main/kotlin/3.kt
+++ src/main/kotlin/3.kt
@@ -6,7 +6,7 @@ import kotlin.io.path.readText
 fun three() {
     val input = Path("src/main/resources/3.txt").readText()
 
-    val result = input.findMuls().sumOfMuls()
+    val result = input.sumOfMuls()
 
     println("Three:\t\t $result")
 }
@@ -14,21 +14,20 @@ fun three() {
 fun threeHard() {
     val input = Path("src/main/resources/3.txt").readText()
 
-    val result = input.split("do").sumOf { doGroup ->
-        if (!doGroup.contains("n't")) {
-            return@sumOf doGroup.findMuls().sumOfMuls()
-        }
-
-        return@sumOf 0
+    val result = input.split("do").filter { !it.contains("n't") }.sumOf { doGroup ->
+        doGroup.sumOfMuls()
     }
 
     println("Three hard $result")
 }
 
-private fun String.findMuls() = "mul\\(\\d{1,3},\\d{1,3}\\)".toRegex().findAll(this)
-private fun Sequence<MatchResult>.sumOfMuls(): Long {
-    return sumOf {
-        val (first, second) = it.value.substringAfter("(").substringBefore(")").split(",").let { it[0].toLong() to it[1].toLong() }
+private fun String.sumOfMuls(): Long {
+    return "mul\\(\\d{1,3},\\d{1,3}\\)".toRegex().findAll(this).sumOf {
+        val (first, second) = it.value
+            .substringAfter("(")
+            .substringBefore(")")
+            .split(",")
+            .let { it[0].toLong() to it[1].toLong() }
 
         first * second
     }
