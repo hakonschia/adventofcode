47a028900436e2b52271e0cc7ea4d652f6c183ba
Håkon Schia <hakonschia5@gmail.com>
2024-12-07 22:01:55 +0100

    Create an extra function just to not have to send so many parameters everywhere

--- src/main/kotlin/7.kt
+++ src/main/kotlin/7.kt
@@ -18,7 +18,7 @@ fun seven() {
 
     val (sum, time) = measureTimedValue {
         testValuesAndEquations.sumOf { (testValue, equation) ->
-            if (isValid(equation = equation, wantedSum = testValue, sumSoFar = equation.first(), index = 1, concatenate = false)) {
+            if (equation.isValid(wantedSum = testValue, concatenate = false)) {
                 testValue
             } else {
                 0
@@ -42,7 +42,7 @@ fun sevenHard() {
 
     val (sum, time) = measureTimedValue {
         testValuesAndEquations.sumOf { (testValue, equation) ->
-            if (isValid(equation = equation, wantedSum = testValue, sumSoFar = equation.first(), index = 1, concatenate = true)) {
+            if (equation.isValid(wantedSum = testValue, concatenate = true)) {
                 testValue
             } else {
                 0
@@ -53,58 +53,30 @@ fun sevenHard() {
     println("Sevenhard $sum $time")
 }
 
-private fun isValid(
-    equation: List<Long>,
-    wantedSum: Long,
-    sumSoFar: Long,
-    index: Int,
-    concatenate: Boolean
-): Boolean {
-    if (index == equation.size) {
-        return sumSoFar == wantedSum
+private fun List<Long>.isValid(wantedSum: Long, concatenate: Boolean): Boolean {
+    fun isValid(
+        sumSoFar: Long,
+        index: Int
+    ): Boolean {
+        if (index == size) {
+            return sumSoFar == wantedSum
+        }
+
+        val currentValue = get(index)
+
+        if (isValid(sumSoFar = sumSoFar + currentValue, index = index + 1)) return true
+        if (isValid(sumSoFar = sumSoFar * currentValue, index = index + 1)) return true
+
+        // Not necessary as it will be caught in the next iteration
+        // But makes it run significantly faster since we avoid some unnecessary string to long conversions
+        if (sumSoFar > wantedSum) return false
+        if (concatenate && isValid(sumSoFar = "$sumSoFar$currentValue".toLong(), index = index + 1)) return true
+
+        return false
     }
 
-    val currentValue = equation[index]
-
-    if (
-        isValid(
-            equation = equation,
-            sumSoFar = sumSoFar + currentValue,
-            index = index + 1,
-            wantedSum = wantedSum,
-            concatenate = concatenate
-        )
-    ) {
-        return true
-    }
-
-    if (
-        isValid(
-            equation = equation,
-            sumSoFar = sumSoFar * currentValue,
-            index = index + 1,
-            wantedSum = wantedSum,
-            concatenate = concatenate
-        )
-    ) {
-        return true
-    }
-
-    // Makes it run "a lot" faster since we avoid the string to long conversion
-    if (sumSoFar > wantedSum) return false
-
-    if (
-        concatenate &&
-        isValid(
-            equation = equation,
-            sumSoFar = "$sumSoFar$currentValue".toLong(),
-            index = index + 1,
-            wantedSum = wantedSum,
-            concatenate = concatenate
-        )
-    ) {
-        return true
-    }
-
-    return false
+    // Either send first() with index = 1, or 0 and 0 and handle if (index == 0) for the multiplication case
+    // But 0 and 0 is a lot slower!
+    return isValid(sumSoFar = first(), index = 1)
 }
+
