7e73ceaeb439d6bff078c26ab0b886084cb42d8e
Håkon Schia <hakonschia5@gmail.com>
2024-12-08 08:49:31 +0100

    Eight hard

--- src/main/kotlin/8.kt
+++ src/main/kotlin/8.kt
@@ -52,4 +52,64 @@ fun eight() {
 fun eightHard() {
     val input = Path("src/main/resources/8.txt").readLines()
 
+    val map = input.map { it.toCharArray() }.toTypedArray()
+
+    val positions = mutableMapOf<Char, MutableList<Pair<Int, Int>>>()
+
+    map.forEachIndexed { outerIndex, charArray ->
+        charArray.forEachIndexed { innerIndex, char ->
+            if (char != '.') {
+                val list = positions.getOrDefault(char, mutableListOf())
+
+                list.add(outerIndex to innerIndex)
+
+                positions[char] = list
+            }
+        }
+    }
+
+    val positionsAdded = mutableListOf<Pair<Char, Pair<Int, Int>>>()
+
+    positions.forEach { (key, innerPositions) ->
+        innerPositions.forEach { outer ->
+            innerPositions.forEach { inner ->
+                if (outer != inner) {
+                    val distance = (outer.first - inner.first) to (outer.second - inner.second)
+
+                    for (i in 1 until 100) {
+                        val antiNodeA = (outer.first + (distance.first * i)) to (outer.second + (distance.second * i))
+                        val antiNodeB = inner.first + (distance.first * i) to inner.second + (distance.second * i)
+
+                        positionsAdded.add(key to antiNodeA)
+                        positionsAdded.add(key to antiNodeB)
+                    }
+                }
+            }
+        }
+    }
+
+    val positionsFiltered = positionsAdded
+        .filter { (_, position) -> position.first >= 0 && position.first < map.size && position.second >= 0 && position.second < map.size }
+        .filterIndexed { index, (key, position) ->
+            map[position.first][position.second] == '.' || map[position.first][position.second] != key
+        }
+        .distinctBy { it.second }
+
+    val safeToEditMap = input.map { it.toCharArray() }.toTypedArray()
+
+    positionsFiltered.forEachIndexed { index, pair ->
+        val position = pair.second
+        safeToEditMap[position.first][position.second] = '#'
+    }
+
+    var c = 0
+    safeToEditMap.forEachIndexed { index, chars ->
+        chars.forEach { char ->
+            if (char != '#' && char != '.') {
+                c++
+            }
+        }
+    }
+
+    println("Eight ${positionsFiltered.size + c} $c")
 }
