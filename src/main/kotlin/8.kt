eeeed1e55e14bf7bf9009954544f0c058088fe0f
Håkon Schia <hakonschia5@gmail.com>
2024-12-08 08:57:48 +0100

    Make common function for easy and hard

--- src/main/kotlin/8.kt
+++ src/main/kotlin/8.kt
@@ -1,50 +1,11 @@
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
-import kotlin.math.abs
 
 fun eight() {
     val input = Path("src/main/resources/8.txt").readLines()
 
     val map = input.map { it.toCharArray() }.toTypedArray()
-
-    val positions = mutableMapOf<Char, MutableList<Pair<Int, Int>>>()
-
-    map.forEachIndexed { outerIndex, charArray ->
-        charArray.forEachIndexed { innerIndex, char ->
-            if (char != '.') {
-                val list = positions.getOrDefault(char, mutableListOf())
-
-                list.add(outerIndex to innerIndex)
-
-                positions[char] = list
-            }
-        }
-    }
-
-    val positionsAdded = mutableListOf<Pair<Char, Pair<Int, Int>>>()
-
-    positions.forEach { (key, innerPositions) ->
-        innerPositions.forEach { outer ->
-            innerPositions.forEach { inner ->
-                if (outer != inner) {
-                    val distance = (outer.first - inner.first) to (outer.second - inner.second)
-
-                    val antiNodeA = (outer.first + distance.first) to (outer.second + distance.second)
-                    val antiNodeB = inner.first + distance.first to inner.second + distance.second
-
-                    positionsAdded.add(key to antiNodeA)
-                    positionsAdded.add(key to antiNodeB)
-                }
-            }
-        }
-    }
-
-    val positionsFiltered = positionsAdded
-        .filter { (_, position) -> position.first >= 0 && position.first < map.size && position.second >= 0 && position.second < map.size }
-        .filterIndexed { index, (key, position) ->
-            map[position.first][position.second] == '.' || map[position.first][position.second] != key
-        }
-        .distinctBy { it.second }
+    val positionsFiltered = map.findPositions(false)
 
     println("Eight ${positionsFiltered.size}")
 }
@@ -53,8 +14,30 @@ fun eightHard() {
     val input = Path("src/main/resources/8.txt").readLines()
 
     val map = input.map { it.toCharArray() }.toTypedArray()
+    val safeToEditMap = input.map { it.toCharArray() }.toTypedArray()
 
+    val positionsFiltered = map.findPositions(true)
+
+    positionsFiltered.forEachIndexed { index, pair ->
+        val position = pair.second
+        safeToEditMap[position.first][position.second] = '#'
+    }
+
+    var additionalCount = 0
+    safeToEditMap.forEachIndexed { index, chars ->
+        chars.forEach { char ->
+            if (char != '#' && char != '.') {
+                additionalCount++
+            }
+        }
+    }
+
+    println("Eight ${positionsFiltered.size + additionalCount} $additionalCount")
+}
+
+private fun Array<CharArray>.findPositions(hard: Boolean): List<Pair<Char, Pair<Int, Int>>> {
     val positions = mutableMapOf<Char, MutableList<Pair<Int, Int>>>()
+    val map = this
 
     map.forEachIndexed { outerIndex, charArray ->
         charArray.forEachIndexed { innerIndex, char ->
@@ -76,7 +59,10 @@ fun eightHard() {
                 if (outer != inner) {
                     val distance = (outer.first - inner.first) to (outer.second - inner.second)
 
-                    for (i in 1 until 100) {
+                    // Will add tons of out-of-bounds nodes, will just remove them later
+                    val iterations = if (hard) map.size else 1
+
+                    for (i in 1 until iterations + 1) {
                         val antiNodeA = (outer.first + (distance.first * i)) to (outer.second + (distance.second * i))
                         val antiNodeB = inner.first + (distance.first * i) to inner.second + (distance.second * i)
 
@@ -88,28 +74,10 @@ fun eightHard() {
         }
     }
 
-    val positionsFiltered = positionsAdded
+    return positionsAdded
         .filter { (_, position) -> position.first >= 0 && position.first < map.size && position.second >= 0 && position.second < map.size }
         .filterIndexed { index, (key, position) ->
             map[position.first][position.second] == '.' || map[position.first][position.second] != key
         }
         .distinctBy { it.second }
-
-    val safeToEditMap = input.map { it.toCharArray() }.toTypedArray()
-
-    positionsFiltered.forEachIndexed { index, pair ->
-        val position = pair.second
-        safeToEditMap[position.first][position.second] = '#'
-    }
-
-    var c = 0
-    safeToEditMap.forEachIndexed { index, chars ->
-        chars.forEach { char ->
-            if (char != '#' && char != '.') {
-                c++
-            }
-        }
-    }
-
-    println("Eight ${positionsFiltered.size + c} $c")
 }
